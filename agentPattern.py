beaa1a6163f61e533890d447d4b69c065b750395
HouKC <2015130149@email.szu.edu.cn>
2020-01-14 17:35:00 +0800

    update agentPattern.py

--- agentPattern.py
+++ agentPattern.py
@@ -1,35 +1,100 @@
 # -*- coding:utf-8 -*-
 
 
-# 代理模式示例：演员经纪人制作公司
-class Actor(object):
+# # 代理模式示例：演员经纪人制作公司
+# class Actor(object):
+#     def __init__(self):
+#         self.isBusy = False
+#
+#     def occupied(self):
+#         self.isBusy = True
+#         print(type(self).__name__, "is occupied with current movie")
+#
+#     def available(self):
+#         self.isBusy = False
+#         print(type(self).__name__, "is free for the movie")
+#
+#     def getStatus(self):
+#         return self.isBusy
+#
+#
+# class Agent(object):
+#     def __init__(self):
+#         self.principal = None
+#
+#     def work(self):
+#         self.actor = Actor()
+#         if self.actor.getStatus():
+#             self.actor.occupied()
+#         else:
+#             self.actor.available()
+#
+#
+# if __name__ == "__main__":
+#     r = Agent()
+#     r.work()
+
+
+# 代理模式实例：银行借记卡
+from abc import ABCMeta, abstractmethod
+
+
+class Payment(metaclass=ABCMeta):
+    @abstractmethod
+    def do_pay(self):
+        pass
+
+
+class Bank(Payment):
     def __init__(self):
-        self.isBusy = False
+        self.card = None
+        self.account = None
 
-    def occupied(self):
-        self.isBusy = True
-        print(type(self).__name__, "is occupied with current movie")
+    def __getAccount(self):
+        self.account = self.card    # Assume card number is account number
+        return self.account
 
-    def available(self):
-        self.isBusy = False
-        print(type(self).__name__, "is free for the movie")
+    def __hasFunds(self):
+        print("Bank:: Checking if Account", self.__getAccount(), "has enough funds")
+        return True
 
-    def getStatus(self):
-        return self.isBusy
+    def setCard(self, card):
+        self.card = card
 
-
-class Agent(object):
-    def __init__(self):
-        self.principal = None
-
-    def work(self):
-        self.actor = Actor()
-        if self.actor.getStatus():
-            self.actor.occupied()
+    def do_pay(self):
+        if self.__hasFunds():
+            print("Bank:: Paying the merchant")
+            return True
         else:
-            self.actor.available()
+            print("Bank:: Sorry, not enough funds! ")
+            return False
 
 
-if __name__ == "__main__":
-    r = Agent()
-    r.work()
+class DebitCard(Payment):
+    def __init__(self):
+        self.bank = Bank()
+
+    def do_pay(self):
+        card = input("Proxy:: Punch in Card Number: ")
+        self.bank.setCard(card)
+        return self.bank.do_pay()
+
+
+class You:
+    def __init__(self):
+        print("You:: Lets buy the Denim shirt")
+        self.debitCard = DebitCard()
+        self.isPuchased = None
+
+    def make_payment(self):
+        self.isPuchased = self.debitCard.do_pay()
+
+    def __del__(self):
+        if self.isPuchased:
+            print("You:: Wow! Denim shirt is Mine :-)")
+        else:
+            print("You:: I should earn more :(")
+
+
+you = You()
+you.make_payment()
